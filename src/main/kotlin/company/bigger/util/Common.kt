1f57783daef1fe8562656ef6327a37f5b3beb9c7
David Podhola <david@podhola.net>
2018-11-12 22:42:01 +0100

    linting

--- src/main/kotlin/company/bigger/util/Common.kt
+++ src/main/kotlin/company/bigger/util/Common.kt
@@ -1,8 +1,5 @@
 package company.bigger.util
 
-import java.sql.Connection
-import com.zaxxer.hikari.HikariDataSource
-
 internal fun <T> String.asResource(work: (String) -> T): T {
     val content = Ini::class.java.getResource(this).readText()
     return work(content)
