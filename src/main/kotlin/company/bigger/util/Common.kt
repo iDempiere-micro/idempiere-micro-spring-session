3e86ca1b10e18a2585544e463ce3988677a802cd
David Podhola <david@podhola.net>
2018-11-12 22:36:41 +0100

    cleanup

--- src/main/kotlin/company/bigger/util/Common.kt
+++ src/main/kotlin/company/bigger/util/Common.kt
@@ -7,11 +7,3 @@ internal fun <T> String.asResource(work: (String) -> T): T {
     val content = Ini::class.java.getResource(this).readText()
     return work(content)
 }
-
-fun getConnectionRO(): Connection {
-    val ds = HikariDataSource()
-    ds.jdbcUrl = "jdbc:mysql://localhost:3306/simpsons"
-    ds.username = "bart"
-    ds.password = "51mp50n"
-    return ds.connection
-}
