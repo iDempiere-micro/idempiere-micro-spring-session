2a5620de4d7eabcc36c9fc2a00a8505d8636615e
David Podhola <david@podhola.net>
2018-11-13 21:16:31 +0100

    cleanup + code comments

--- src/main/kotlin/company/bigger/dto/UserLoginModelResponse.kt
+++ src/main/kotlin/company/bigger/dto/UserLoginModelResponse.kt
@@ -1,9 +1,28 @@
 package company.bigger.dto
 
+/**
+ * The response we send to the calling site as a result of an attempted login.
+ * See [ILoginResponse] for the details and limitations
+ */
 data class UserLoginModelResponse(
+    /**
+     * Was the request authenticated?
+      */
     override val logged: Boolean = false,
+    /**
+     * Then token to be then sent with the requests
+     */
     override val token: String? = null,
+    /**
+     * The loginName that was used to send the login request
+     */
     override val loginName: String,
-    override val clientId: Int = -1,
-    override val userId: Int = -1
+    /**
+     * The clientId of the authenticated user or null
+     */
+    override val clientId: Int? = null,
+    /**
+     * The authenticated user Id or null
+     */
+    override val userId: Int? = null
 ) : ILoginResponse
