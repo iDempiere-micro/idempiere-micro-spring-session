ea6f94264d2baa1776bfe1c2d2af065d6da81cda
David Podhola <david@podhola.net>
2018-11-13 23:18:13 +0100

    circleci

---
+++ tools/idempiere_db.sh
@@ -0,0 +1,6 @@
+if [ ! -f idempiere-docker-pgsql-5.1.0.latest.tar.gz ]; then
+    wget https://sourceforge.net/projects/idempiereksys/files/idempiere-docker-image/idempiere-docker-pgsql-5.1.0.latest.tar.gz
+fi
+zcat idempiere-docker-pgsql-5.1.0.latest.tar.gz | docker load
+docker volume create --name idempiere-pgsql-datastore
+docker run -d -v idempiere-pgsql-datastore:/data -p 5433:5432 -e PASS="postgres" idempiere/idempiere-docker-pgsql:5.1.0.latest
