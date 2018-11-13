7ee14749828703060f3b91cdaa827a319d8e5277
David Podhola <david@podhola.net>
2018-11-13 19:57:40 +0100

    cleanup + code comments

--- src/main/kotlin/company/bigger/dto/UserLoginModel.kt
+++ src/main/kotlin/company/bigger/dto/UserLoginModel.kt
@@ -2,10 +2,19 @@ package company.bigger.dto
 
 /**
  * A data class to send the login parameters.
- * See [ILogin] for limitations.
+ * See [ILogin] for notes and limitations.
  */
 data class UserLoginModel(
+    /**
+     * The user name or email to log in with.
+     */
     override val loginName: String,
+    /**
+     * The unencrypted password. Always use SSL!
+     */
     override val password: String,
+    /**
+     * The non mandatory clientId necessary to be sent only if the user has access to more clients.
+     */
     override val clientId: Int? = null
 ) : ILogin
--- src/main/kotlin/company/bigger/dto/UserLoginModelResponse.kt
+++ src/main/kotlin/company/bigger/dto/UserLoginModelResponse.kt
@@ -6,24 +6,4 @@ data class UserLoginModelResponse(
     override val loginName: String,
     override val clientId: Int = -1,
     override val userId: Int = -1
-) : ILoginResponse {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as UserLoginModelResponse
-
-        if (logged != other.logged) return false
-        if (token != other.token) return false
-        if (loginName != other.loginName) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = logged.hashCode()
-        result = 31 * result + (token?.hashCode() ?: 0)
-        result = 31 * result + (loginName.hashCode())
-        return result
-    }
-}
+) : ILoginResponse
